4cd58b78062de444edd7cbd148e8a94a4f339094
mateusfrazz <mateusfrazao12345@gmail.com>
2024-11-29 15:42:04 -0300

    finalizado a classe contaCorrente e inciado a classe endereços

--- src/modules/cliente.ts
+++ src/modules/cliente.ts
@@ -14,5 +14,6 @@ export class Cliente extends Pessoa{
     }
 
     listarEnderecos():void{
+        
     }
 }
---
+++ src/modules/contaCorrente.ts
@@ -0,0 +1,9 @@
+import { Conta } from "./conta";
+
+class ContaCorrente extends Conta{
+    calcularSaldo():void{}
+    constructor(numero:string){
+        super(numero)
+     }
+
+    }
