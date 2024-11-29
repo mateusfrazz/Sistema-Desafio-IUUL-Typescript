7d6fece32e46abfe578ccd576b3b21999ca41225
mateusfrazz <mateusfrazao12345@gmail.com>
2024-11-29 15:10:35 -0300

    finalizada a estrutura da classe Conta

---
+++ src/modules/conta.ts
@@ -0,0 +1,14 @@
+export abstract class Conta{
+    numero:string;
+    constructor(numero:string){
+    this.numero = numero;
+    }
+
+    depositar(valorDeposito:number):void{
+
+    }
+
+    sacar(valorSaque:number):void{
+        
+    }
+}
