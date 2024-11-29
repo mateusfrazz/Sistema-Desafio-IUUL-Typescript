0ad2fec889a9836925e9acbad4cb7adcf8c274a8
mateusfrazz <mateusfrazao12345@gmail.com>
2024-11-29 15:19:07 -0300

    finalizado a classe cliente herdada da classe pessoa

---
+++ src/modules/cliente.ts
@@ -0,0 +1,19 @@
+//importando a classe Pessoa
+import { Pessoa } from "./pessoa";
+
+export class Cliente extends Pessoa{
+    protected vip: boolean
+    constructor(
+        cpf:string,
+        nome:string,
+        telefone:string,
+        vip:boolean,
+    ){
+        super(cpf,nome,telefone);
+        this.vip = vip;
+    }
+
+    listarEnderecos():void{
+
+    }
+}
